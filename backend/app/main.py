05afa919ef74dd13dcd0d49eff75da51d55a042b
PRAVEENM282 <praveenm0088@gmail.com>
2025-12-20 01:37:15 +0530

    Refactor CORS middleware settings to allow all origins and clean up imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 from app.routers import upload, analyze, generate
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.database import engine
+from app.models.base import Base
+
+from app.models import media, analysis, generation
+
+Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI(title="AI Vision App")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow your Next.js app
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (POST, GET, etc.)
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
